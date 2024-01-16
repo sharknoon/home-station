cac40cfcbce2249404b880ede1ce20038c5a9ff5
Josua Frank <frank.josua@gmail.com>
2024-01-16 22:27:56 +0100

    Add @sveltejs/adapter-auto to package.json and update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-node';
+import adapterAuto from '@sveltejs/adapter-auto';
+import adapterNode from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +9,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: process.env.ADAPTER === 'AUTO' ? adapterAuto() : adapterNode()
 	}
 };
 
