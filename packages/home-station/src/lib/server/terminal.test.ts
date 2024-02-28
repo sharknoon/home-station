8df050b23629c4b6b6c4485956a5eecc5f68a3e3
Josua Frank <frank.josua@gmail.com>
2024-02-28 12:21:05 +0000

    terminal

---
+++ packages/home-station/src/lib/server/terminal.test.ts
@@ -0,0 +1,8 @@
+import { describe, it } from 'vitest';
+import { exec } from './terminal';
+
+describe('testTerminal', () => {
+    it('should successfully connect to Docker', async () => {
+        exec("")
+    })
+});
