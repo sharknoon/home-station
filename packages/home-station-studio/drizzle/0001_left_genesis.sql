524bae66d32214657920e19d55ccbdfc1e5abc73
Josua Frank <frank.josua@gmail.com>
2024-03-04 22:58:48 +0100

    update db

---
+++ packages/home-station-studio/drizzle/0001_left_genesis.sql
@@ -0,0 +1,17 @@
+ALTER TABLE "oauth_account" DROP CONSTRAINT "oauth_account_user_id_user_id_fk";
+--> statement-breakpoint
+ALTER TABLE "session" DROP CONSTRAINT "session_user_id_user_id_fk";
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "oauth_account" ADD CONSTRAINT "oauth_account_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "session" ADD CONSTRAINT "session_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+ALTER TABLE "user" ADD CONSTRAINT "user_email_unique" UNIQUE("email");
