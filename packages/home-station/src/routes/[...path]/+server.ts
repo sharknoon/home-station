a4a4b73c9c261c0b53e83ba1f65d34d69ffcaf2b
Josua Frank <frank.josua@gmail.com>
2024-03-07 10:43:14 +0100

    fixes

--- packages/home-station/src/routes/[...path]/+server.ts
+++ packages/home-station/src/routes/[...path]/+server.ts
@@ -1,14 +1,17 @@
-import path from 'node:path';
-import { getAppDataPath } from '$lib/server/appdata';
-import { error } from '@sveltejs/kit';
 import fs from 'node:fs/promises';
+import path from 'node:path';
+import { error } from '@sveltejs/kit';
+import mime from 'mime';
+import { getAppDataPath } from '$lib/server/appdata';
 
 export const GET = async ({ params }) => {
     try {
         const appDataPath = await getAppDataPath();
         const filePath = path.join(appDataPath, params.path);
         const file = await fs.readFile(filePath);
-        return new Response(file);
+        return new Response(file, {
+            headers: { 'Content-Type': mime.getType(filePath) ?? 'application/octet-stream' }
+        });
     } catch (e) {
         return error(404);
     }
--- packages/home-station/svelte.config.js
+++ packages/home-station/svelte.config.js
@@ -1,5 +1,4 @@
-import adapterAuto from '@sveltejs/adapter-auto';
-import adapterNode from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,7 +8,7 @@ const config = {
     preprocess: vitePreprocess(),
 
     kit: {
-        adapter: process.env.ADAPTER === 'AUTO' ? adapterAuto() : adapterNode()
+        adapter: adapter()
     }
 };
 
